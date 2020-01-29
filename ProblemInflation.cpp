b9c2cd1952ea28abda8a55fccc9aa59add2655ee
kahnbenjamina <bkahn@chapman.edu>
2020-01-29 07:12:17 +0000

    Added fixed and setprecision() for accurate cents reporting. Changed some wording for better understanding and added comments.

--- ProblemInflation.cpp
+++ ProblemInflation.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip> //for use with fixed and setprecision()
 using namespace std;
 
 int main(int argc, char **argv)
@@ -9,7 +10,7 @@ int main(int argc, char **argv)
 
   cout << "What is the current cost of the item? (Do not include the dollar sign.)" << endl;
   cin >> itemCost; //gets the cost of the item from the user
-  cout << "How many years from now do you expect to buy or sell the item?" << endl;
+  cout << "How many whole years from now do you expect to buy or sell the item?" << endl;
   cin >> numYears; //gets the number of years from the user
   cout << "What is the rate of inflation? (Do not include the percent sign.)" << endl;
   cin >> inflationRate; //gets the inflation rate from the user (in percent form)
@@ -21,7 +22,7 @@ int main(int argc, char **argv)
     itemCost = itemCost * (inflationRate); //update the item cost to account for inflation
   }
 
-  cout << "In " << numYears << " years the item will cost $" << itemCost << "." << endl;
+  cout << "In " << numYears << " years the item will cost $" << fixed << setprecision(2) << itemCost << "." << endl; //accurate cents reporting
 
   return 0;
 }
