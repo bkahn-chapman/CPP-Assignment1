d67968be868df4c1feb6faffacbcf1e8ec67b724
kahnbenjamina <bkahn@chapman.edu>
2020-01-29 07:05:44 +0000

    hoursWeek from int to const unsigned int. hoursWorked from int to float. Added the ability to use fixed and setprecision() for accurate cents reporting. Made comments more accurate.

--- ProblemEmployeePay.cpp
+++ ProblemEmployeePay.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <iomanip> //for use with fixed and setprecision()
 using namespace std;
 
 int main(int argc, char **argv)
 {
   float employeePay = 16.00; //pay for normal hours
-  int hoursWeek = 40; //standard number of hours worked in a week
-  int hoursWorked; //user input for hours worked
+  const unsigned int hoursWeek = 40; //standard number of hours worked in a week
+  float hoursWorked; //user input for hours worked (includes possibility for partial hours)
   float ssTax; //social security tax
   float federalTax; //federal income tax
   float stateTax; //state income tax
@@ -16,20 +17,18 @@ int main(int argc, char **argv)
   cin >> hoursWorked; //gets the number of hours worked in from the user
 
   grossPay = (hoursWeek * employeePay) + ((hoursWorked-hoursWeek) * (employeePay * 1.5)); //determines the overall grosspay for the amount of hours worked
-  cout << "The gross pay this week was $" << grossPay << "." << endl;
+  cout << "The gross pay this week was $" << fixed << setprecision(2) << grossPay << "." << endl; //accurate cents reporting
 
   ssTax = grossPay * 0.06; //calculates social security tax
-  cout << "$" << ssTax << " was withheld for Social Security tax." << endl;
+  cout << "$" << fixed << setprecision(2) << ssTax << " was withheld for Social Security tax." << endl; //accurate cents reporting
   federalTax = grossPay * 0.14; //calculates federal income tax
-  cout << "$" << federalTax << " was withheld for federal income tax." << endl;
+  cout << "$" << fixed << setprecision(2) << federalTax << " was withheld for federal income tax." << endl; //accurate cents reporting
   stateTax = grossPay * 0.05; //calculates state income tax
-  cout << "$" << stateTax << " was withheld for state income tax." << endl;
-  cout << "$10 was withheld for medical insurance." << endl;
+  cout << "$" << fixed << setprecision(2) << stateTax << " was withheld for state income tax." << endl; //accurate cents reporting
+  cout << "$10.00 was withheld for medical insurance." << endl;
 
   netPay = grossPay - ssTax - federalTax - stateTax - 10; //calcualtes the net pay for the week
-  cout << "The overall net take-home pay for the week was $" << netPay << "." << endl;
+  cout << "The overall net take-home pay for the week was $" << fixed << setprecision(2) << netPay << "." << endl; //accurate cents reporting
 
   return 0;
 }
-
-//FIX THE ISSUE WHERE DOLLAR AMOUNTS DON'T ALWAYS HAVE TWO DECIMAL PLACES
